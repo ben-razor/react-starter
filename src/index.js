d190fbdac8866e0c692181ab7083787fe1ae9c3f
Ben Razor <chrisbenwar1@googlemail.com>
2022-01-03 13:49:13 +0000

    Add ui tools

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
-import React, {createRef} from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './css/styles.css';
-import ImageBenRazorHead from './images/br-head-1.png';
+import { ToastProvider, useToasts } from 'react-toast-notifications';
+import Logo from './images/logo-1.png';
+
+const TOAST_TIMEOUT = 4000;
 
 function App(props) {
+  const { addToast } = useToasts();
+
+  function toast(message, type='info') {
+    addToast(message, { 
+      appearance: type,
+      autoDismiss: true,
+      autoDismissTimeout: TOAST_TIMEOUT
+    });
+  }
+
+  function drinkMe() {
+    toast(<Fragment>
+      <div>What a curious feeling!</div>
+      <div>I must be shutting up like a telescope.</div>
+    </Fragment>);
+  }
+
   return (
-    <div>
-      <img alt="Ben Razor Head" src={ImageBenRazorHead} />
-      <h1>Welcome to REACT</h1>
+    <div className="br-page">
+      <div className="br-header">
+        <img className="br-header-logo" alt="Ben Razor Head" src={Logo} />
+        <h1>Welcome to REACT</h1>
+      </div>
+      <div className="br-content">
+        <button onClick={drinkMe}>Drink Me</button>
+      </div>
     </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ToastProvider>
+    <App />
+  </ToastProvider>
+, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
+const webpack = require('webpack')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
+let config = {
     mode: 'development',
     entry: {
         'index': '/src/index.js',
@@ -31,14 +32,18 @@ module.exports = {
             },
             {
                 test: /\.txt$/,
-                use: ['raw-loader'],
+                type: 'asset/source'
+            },
+            {
+                test: /\.json$/,
+                type: 'asset/source'
             },
             {
                 test: /\.css$/,
                 use: ['style-loader', {
                     loader: 'css-loader',
                     options: {
-                        url: false
+                        url: true 
                     }
                 }]
             },
@@ -47,50 +52,69 @@ module.exports = {
                 use: {
                     loader: 'file-loader',
                     options: {
-                        name: '[name].[hash:6].[ext]'
+                        name: '[name].[hash:6].[ext]',
+                        outputPath: 'wasm/'
                     }
                 },
                 type: 'javascript/auto'
             },
             {
                 test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [
-                  {
-                    loader: 'file-loader',
-                    options: {
-                      name: '[name].[ext]',
-                      outputPath: 'fonts/'
-                    }
-                  }
-                ]
-              },
+                type: 'asset/resource',
+                generator: { filename: 'fonts/[name][hash][ext]' }
+            },
             {
-                test: /\.(png|jpg|gif|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [
-                  {
-                    loader: 'file-loader',
-                    options: {
-                      name: '[name].[ext]',
-                      outputPath: 'images/'
-                    }
-                  }
-                ]
+                test: /\.(png|jpg|gif|svg|ico)(\?v=\d+\.\d+\.\d+)?$/,
+                type: 'asset/resource',
+                generator: { filename: 'images/[name][hash][ext]' }
             }
         ],
     },
     resolve: {
         extensions: ['.js', '.ts', '.tsx'],
+        fallback: {
+            util: require.resolve('util'),
+            path: require.resolve('path-browserify'),
+            stream: require.resolve('stream-browserify'),
+            crypto: require.resolve('crypto-browserify'),
+            http: require.resolve('stream-http'),
+            https: require.resolve('https-browserify'),
+            os: require.resolve("os-browserify/browser"),
+            url: require.resolve('url'),
+            assert: require.resolve('assert'),
+            buffer: require.resolve("buffer")
+        }
     },
     plugins: [
+        // fix "process is not defined" error:
+        // (do "npm install process" before running the build)
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        }),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
         new HTMLWebpackPlugin({
             chunks: ['index'],
             filename: 'index.html',
+            favicon: './src/images/favicon_io/favicon.ico',
             template: path.resolve('./src/index.html')
         }),
         new HTMLWebpackPlugin({
             chunks: ['sandbox'],
             filename: 'sandbox.html',
+            favicon: './src/images/favicon_io/favicon.ico',
             template: path.resolve('./src/sandbox.html')
         })
     ]
 }
+
+let configFunc = (env, options) => {
+    if(options.mode !== 'production') {
+        config['devtool'] = 'eval-source-map';
+    }
+
+    return config;
+}
+
+module.exports = configFunc;
